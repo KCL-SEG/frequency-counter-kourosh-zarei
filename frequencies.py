2513d8f16ca74866eaa5aaa4483fc3cf7afc89d4
kourosh-zarei <126338174+kourosh-zarei@users.noreply.github.com>
2023-09-25 15:06:43 +0100

    feat: working frequency func

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,11 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+from collections import Counter
+
+
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+    items = [str(item) for item in items]
+    _frequencies = Counter(items)
+    return _frequencies
